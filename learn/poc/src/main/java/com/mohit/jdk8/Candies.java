dddd1eff9b724812a7eae34ab1f4f8ea39c65ac6
mohgoel <mohgoel@expedia.com>
2020-03-29 15:20:29 +0530

    linkedList

--- learn/poc/src/main/java/com/mohit/jdk8/Candies.java
+++ learn/poc/src/main/java/com/mohit/jdk8/Candies.java
@@ -4,45 +4,47 @@ import java.util.*;
 
 public class Candies {
 
-    public static void main(String[] args) {
-        int[] status = new int[] { 1, 0, 1, 0 };
+	public static void main(String[] args) {
 
-        int r = maxCandies(new int[] { 1, 0, 1, 0 }, new int[] { 7, 5, 4, 100 }, new int[][] { {}, {}, { 1 }, {} }, new int[][] { { 1, 2 }, { 3 }, {}, {} }, new int[] { 0 });
-         System.out.println(r);
-    }
+		int r = maxCandies(new int[] { 1, 0, 1, 0 }, new int[] { 7, 5, 4, 100 }, new int[][] { {}, {}, { 1 }, {} },
+				new int[][] { { 1, 2 }, { 3 }, {}, {} }, new int[] { 0 });
+		System.out.println(r);
+	}
 
-    public static int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+	public static int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes,
+			int[] initialBoxes) {
 
-        int result = 0;
+		int result = 0;
 
-        Set<Integer> cBoxes = new HashSet<>();
-        Set<Integer> cKeys = new HashSet<>();
-        collectBoxesAndKeys(cBoxes, cKeys, keys, containedBoxes, initialBoxes);
+		Set<Integer> cBoxes = new HashSet<>();
+		Set<Integer> cKeys = new HashSet<>();
+		collectBoxesAndKeys(cBoxes, cKeys, keys, containedBoxes, initialBoxes);
 
-        for (int index = 0; index < status.length; index++) {
-            if (cBoxes.contains(index) && (status[index] == 1 || cKeys.contains(index))) {
-                result += candies[index];
-            }
-        }
+		for (int index = 0; index < status.length; index++) {
+			if (cBoxes.contains(index) && (status[index] == 1 || cKeys.contains(index))) {
+				result += candies[index];
+			}
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    public static void collectBoxesAndKeys(Set<Integer> cBoxes, Set<Integer> cKeys, int[][] keys, int[][] boxes, int[] initBoxes) {
+	public static void collectBoxesAndKeys(Set<Integer> cBoxes, Set<Integer> cKeys, int[][] keys, int[][] boxes,
+			int[] initBoxes) {
 
-        for (int boxNo : initBoxes) {
-            cBoxes.add(boxNo);
-            addItems(cBoxes, boxes[boxNo]);
-            addItems(cKeys, keys[boxNo]);
-            collectBoxesAndKeys(cBoxes, cKeys, keys, boxes, boxes[boxNo]);
-        }
+		for (int boxNo : initBoxes) {
+			cBoxes.add(boxNo);
+			addItems(cBoxes, boxes[boxNo]);
+			addItems(cKeys, keys[boxNo]);
+			collectBoxesAndKeys(cBoxes, cKeys, keys, boxes, boxes[boxNo]);
+		}
 
-    }
+	}
 
-    public static void addItems(Set<Integer> aSet, int[] array) {
-        for (int a : array) {
-            aSet.add(a);
-        }
-    }
+	public static void addItems(Set<Integer> aSet, int[] array) {
+		for (int a : array) {
+			aSet.add(a);
+		}
+	}
 
 }
--- learn/poc/src/main/java/com/mohit/linkedList/SumOfLinkedListNodes.java
+++ learn/poc/src/main/java/com/mohit/linkedList/SumOfLinkedListNodes.java
@@ -3,114 +3,124 @@ package com.mohit.linkedList;
 import com.mohit.poc.pojo.LinkedListNode;
 
 /**
- * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in
- * reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked
- * list.
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order and each of their nodes
+ * contain a single digit. Add the two numbers and return it as a linked list.
  * 
- * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
  * 
  * Example:
  * 
- * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) Output: 7 -> 0 -> 8 Explanation: 342 + 465 = 807.
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) Output: 7 -> 0 -> 8 Explanation: 342 +
+ * 465 = 807.
  *
  * 
  * 
  */
 public class SumOfLinkedListNodes {
 
-    public void execute() {
-        LinkedListNode l1 = new LinkedListNode(2);
-        l1.next = new LinkedListNode(4);
-        l1.next.next = new LinkedListNode(3);
+	public static void main(String[] args) {
+		SumOfLinkedListNodesSol obj = new SumOfLinkedListNodesSol();
+		obj.execute();
+	}
 
-        LinkedListNode l2 = new LinkedListNode(5);
-        l2.next = new LinkedListNode(6);
-        l2.next.next = new LinkedListNode(4);
+}
 
-        printResult(addTwoNumbers(l1, l2));
-        printResult(addTwoNumbersRecursive(l1, l2, 0));
-        
-        l2.next.next.next = new LinkedListNode(2);
-        printResult(addTwoNumbers(l1, l2));
-        printResult(addTwoNumbersRecursive(l1, l2, 0));
-        
-    }
+class SumOfLinkedListNodesSol {
 
-    public LinkedListNode addTwoNumbers(LinkedListNode l1, LinkedListNode l2) {
+	public void execute() {
+		LinkedListNode l1 = new LinkedListNode(2);
+		l1.next = new LinkedListNode(4);
+		l1.next.next = new LinkedListNode(3);
 
-        int sum = 0;
-        int carry = 0;
-        LinkedListNode head = null;
-        LinkedListNode prev = null;
+		LinkedListNode l2 = new LinkedListNode(5);
+		l2.next = new LinkedListNode(6);
+		l2.next.next = new LinkedListNode(4);
 
-        while (l1 != null || l2 != null) {
+		printResult(addTwoNumbers(l1, l2));
+		printResult(addTwoNumbersRecursive(l1, l2, 0));
 
-            int n1 = 0;
-            int n2 = 0;
-            if (l1 != null) {
-                n1 = l1.val;
-                l1 = l1.next;
-            }
+		l2.next.next.next = new LinkedListNode(2);
+		printResult(addTwoNumbers(l1, l2));
+		printResult(addTwoNumbersRecursive(l1, l2, 0));
 
-            if (l2 != null) {
-                n2 = l2.val;
-                l2 = l2.next;
-            }
+	}
 
-            sum = (n1 + n2 + carry);
-            carry = sum / 10;
+	public LinkedListNode addTwoNumbers(LinkedListNode l1, LinkedListNode l2) {
 
-            LinkedListNode node = new LinkedListNode(sum % 10);
+		int sum = 0;
+		int carry = 0;
+		LinkedListNode head = null;
+		LinkedListNode prev = null;
 
-            if (head == null) {
-                head = node;
-                prev = node;
-            } else {
-                prev.next = node;
-                prev = node;
-            }
+		while (l1 != null || l2 != null) {
 
-        }
-        if (carry > 0) {
-            LinkedListNode node = new LinkedListNode(carry);
-            prev.next = node;
-        }
+			int n1 = 0;
+			int n2 = 0;
+			if (l1 != null) {
+				n1 = l1.val;
+				l1 = l1.next;
+			}
 
-        return head;
+			if (l2 != null) {
+				n2 = l2.val;
+				l2 = l2.next;
+			}
 
-    }
+			sum = (n1 + n2 + carry);
+			carry = sum / 10;
 
-    
-    public LinkedListNode addTwoNumbersRecursive(LinkedListNode l1, LinkedListNode l2, int carry) {
-        
-        if(l1 == null && l2 == null) {
-            if(carry > 0) {
-                return new LinkedListNode(carry);
-            }
-            return null;
-        }
-        
-        int v1 = l1 != null ? l1.val : 0;
-        int v2 = l2 != null ? l2.val : 0;
-        
-        int sum = v1 + v2 + carry;
-        carry = sum / 10;
-        
-        LinkedListNode aNode = new LinkedListNode(sum % 10);
-        
-        l1 = l1 != null ? l1.next : null;
-        l2 = l2 != null ? l2.next : null;
-        
-        aNode.next = addTwoNumbersRecursive(l1, l2, carry);
-        
-        return aNode;
-    }
-    
-    private void printResult(LinkedListNode result) {
-        while (result != null) {
-            System.out.print(result.val);
-            result = result.next;
-        }
-        System.out.println();
-    }
+			LinkedListNode node = new LinkedListNode(sum % 10);
+
+			if (head == null) {
+				head = node;
+				prev = node;
+			} else {
+				prev.next = node;
+				prev = node;
+			}
+
+		}
+		if (carry > 0) {
+			LinkedListNode node = new LinkedListNode(carry);
+			prev.next = node;
+		}
+
+		return head;
+
+	}
+
+	public LinkedListNode addTwoNumbersRecursive(LinkedListNode l1, LinkedListNode l2, int carry) {
+
+		if (l1 == null && l2 == null) {
+			if (carry > 0) {
+				return new LinkedListNode(carry);
+			}
+			return null;
+		}
+
+		int v1 = l1 != null ? l1.val : 0;
+		int v2 = l2 != null ? l2.val : 0;
+
+		int sum = v1 + v2 + carry;
+		carry = sum / 10;
+
+		LinkedListNode aNode = new LinkedListNode(sum % 10);
+
+		l1 = l1 != null ? l1.next : null;
+		l2 = l2 != null ? l2.next : null;
+
+		aNode.next = addTwoNumbersRecursive(l1, l2, carry);
+
+		return aNode;
+	}
+
+	private void printResult(LinkedListNode result) {
+		while (result != null) {
+			System.out.print(result.val);
+			result = result.next;
+		}
+		System.out.println();
+	}
 }
