2bf83028915a79ac2409c8a6800883d9c4c5cbe5
mohgoel <mohgoel@expedia.com>
2020-04-26 16:36:19 +0530

    meeting room -- priority queue

---
+++ learn/poc/src/main/java/com/mohit/queue/priority/MeetingRooms2.java
@@ -0,0 +1,87 @@
+package com.mohit.queue.priority;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/*
+ * https://leetcode.com/problems/meeting-rooms-ii/
+ * 
+ * https://www.programcreek.com/2014/05/leetcode-meeting-rooms-ii-java/
+ */
+public class MeetingRooms2 {
+
+	public static void main(String[] args) {
+		MeetingRooms2Sol sol = new MeetingRooms2Sol();
+		assertEquals(2, sol.minMeetingRooms(new int[][] { { 0, 30 }, { 5, 10 }, { 15, 20 } }));
+		System.out.println("All passed " + sol.getClass().getSimpleName());
+	}
+
+}
+
+/**
+ * Sort all intervals in ascending order of start time.
+ * 
+ * Also maintain priority queue which will hold meeting which is going to be finished earliest
+ * 
+ * whenever we get new interval in iterator... 
+ * we check if heap has any meeting which is finished before starting this.
+ * if so..... remove top entry from heap and add this.
+ *
+ */
+class MeetingRooms2Sol {
+
+	public int minMeetingRooms(int[][] intervals) {
+		
+		if(intervals == null || intervals.length < 2) {
+			return 1;
+		}
+		
+		Arrays.sort(intervals, new IntervalSorted());
+		Queue<Integer> minQueue = new PriorityQueue<>(); //Ascending order by default
+		
+		int result = 0;
+		int runningCount = 0;
+		
+		for(int[] interval : intervals) {
+			if(result == 0) {
+				result++;
+				runningCount++;
+				minQueue.add(interval[1]);
+				continue;
+			}
+			
+			int newStart = interval[0];
+			int earliestFinished = minQueue.peek();
+			
+			/*
+			 * If new start	is earlier than earliestFinished
+			 * we need one more room
+			 */
+			if(newStart < earliestFinished) {
+				runningCount++;
+				result = Math.max(runningCount, result);
+			} else {
+				minQueue.poll();
+			}
+			minQueue.add(interval[1]);
+			
+		}
+		return result;
+	}
+
+}
+
+/*
+ * Ascending order on start time
+ */
+class IntervalSorted implements Comparator<int[]> {
+	
+	public int compare(int[] a, int[] b) {
+		return a[0] - b[0];
+	}
+	
+}
